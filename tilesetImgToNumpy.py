13122db0e76290c77f8c89d4f669011498e2e8ba
1000000000 <lthyde@gmail.com>
2015-03-19 10:31:19 -0400

    Fixed bug in tilesetImgToNumpy and made it skip redundant tiles
    When resizing the numpy array of the image to correct dimensions
    after converting it the dimensions were swapped

--- tilesetImgToNumpy.py
+++ tilesetImgToNumpy.py
@@ -1,5 +1,5 @@
 import numpy as np
-from imageConvert import TILE_WIDTH, TILE_HEIGHT
+from imageConvert import TILE_WIDTH, TILE_HEIGHT, REDUNDANT_TILES
 import imageConvert as convert
 from PIL import Image
 
@@ -9,11 +9,15 @@ way for the imageConvert program and saves the array to a file.
 '''
 def convertToNumpy(tileset):
 	tilesetSize = (tileset.size[0]/TILE_WIDTH, tileset.size[1]/TILE_HEIGHT)
-	tilesetArray = convert.cropImage(np.array(tileset.getdata()).astype(bool).reshape(tileset.size))[0]
-	newArray = np.empty((TILE_WIDTH*TILE_HEIGHT,tilesetSize[0]*tilesetSize[1]), dtype=bool)
+	tilesetArray = convert.cropImage(np.array(tileset.getdata()).astype(bool).reshape((tileset.size[1],tileset.size[0])))[0]
+	newArray = np.empty((TILE_WIDTH*TILE_HEIGHT,tilesetArray.shape[0]-len(REDUNDANT_TILES)), dtype=bool)
 	for p in range(TILE_WIDTH*TILE_HEIGHT):
+		offset = 0
 		for t in range(tilesetArray.shape[0]):
-			newArray[p,t] = tilesetArray[t].flat[p]
+			if not t in REDUNDANT_TILES:
+				newArray[p,t-offset] = tilesetArray[t].flat[p]
+			else:
+				offset += 1
 	np.save("resources/tileset.npy", newArray)
 
 if __name__ == "__main__":
